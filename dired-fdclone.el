2c911aaee8ad18ce7c92346ba452229a5d7fc1ed
Akinori MUSHA <knu@idaemons.org>
2024-12-06 20:55:11 +0900

    Bump the version to 1.6.4

--- dired-fdclone.el
+++ dired-fdclone.el
@@ -28,7 +28,7 @@
 ;; Author: Akinori MUSHA <knu@iDaemons.org>
 ;; URL: https://github.com/knu/dired-fdclone.el
 ;; Created: 25 Dec 2014
-;; Version: 1.6.3
+;; Version: 1.6.4
 ;; Keywords: unix, directories, dired
 
 ;;; Commentary:
