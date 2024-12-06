f8b06d328dcaa3c77543c92f0ab950988cd1f65d
Akinori MUSHA <knu@idaemons.org>
2024-12-07 00:47:15 +0900

    Provide dired-fdclone.el for backward compatibility

---
+++ dired-fdclone.el
@@ -0,0 +1,43 @@
+;;; dired-fdclone.el --- dired functions and settings to mimic FD/FDclone
+;;
+;; Copyright (c) 2014-2024 Akinori MUSHA
+;;
+;; All rights reserved.
+;;
+;; Redistribution and use in source and binary forms, with or without
+;; modification, are permitted provided that the following conditions
+;; are met:
+;; 1. Redistributions of source code must retain the above copyright
+;;    notice, this list of conditions and the following disclaimer.
+;; 2. Redistributions in binary form must reproduce the above copyright
+;;    notice, this list of conditions and the following disclaimer in the
+;;    documentation and/or other materials provided with the distribution.
+;;
+;; THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+;; ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;; ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+;; FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+;; DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+;; OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+;; HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+;; LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+;; OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+;; SUCH DAMAGE.
+
+;; Author: Akinori MUSHA <knu@iDaemons.org>
+;; URL: https://github.com/knu/diredfd.el
+;; Created: 25 Dec 2014
+;; Version: 2.0.0
+;; Keywords: unix, directories, dired
+
+;;; Code:
+
+(require 'diredfd)
+
+;;;###autoload
+(defalias 'dired-fdclone #'diredfd-enable)
+
+(provide 'dired-fdclone)
+
+;;; dired-fdclone.el ends here
